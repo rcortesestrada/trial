a460ea8293dc2ceedd7fd6aa4a51bd98dcd3cb7a
rcortesestrada <rcortesestrada@mail.dccc.edu>
2026-04-28 19:23:39 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Listening at ${port}`);    
